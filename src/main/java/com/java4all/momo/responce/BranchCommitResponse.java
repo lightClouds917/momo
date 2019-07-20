e78f19e2d0fe35de722dc0b509239ce6fda62599
ITyunqing <1186355422@qq.com>
2019-07-20 23:31:47 +0800

    update annotation

--- src/main/java/com/java4all/momo/responce/BranchCommitResponse.java
+++ src/main/java/com/java4all/momo/responce/BranchCommitResponse.java
@@ -1,8 +1,8 @@
 package com.java4all.momo.responce;
 
 /**
- * @author wangzhongxiang
- * @date 2019年07月20日 23:19:14
+ * Branch commit response
+ * @author IT云清
  */
 public class BranchCommitResponse {
 
--- src/main/java/com/java4all/momo/responce/BranchRollbackResponse.java
+++ src/main/java/com/java4all/momo/responce/BranchRollbackResponse.java
@@ -1,8 +1,8 @@
 package com.java4all.momo.responce;
 
 /**
- * @author wangzhongxiang
- * @date 2019年07月20日 23:19:41
+ * Branch rollback response
+ * @author IT云清
  */
 public class BranchRollbackResponse {
 
