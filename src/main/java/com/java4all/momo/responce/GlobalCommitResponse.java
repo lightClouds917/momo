a53a690b0d4c35e6beb6f8b2557061366a13261a
ITyunqing <1186355422@qq.com>
2019-07-26 22:34:58 +0800

    add reponse

---
+++ src/main/java/com/java4all/momo/responce/GlobalCommitResponse.java
@@ -0,0 +1,9 @@
+package com.java4all.momo.responce;
+
+/**
+ * Global commit response
+ * @author IT云清
+ */
+public class GlobalCommitResponse {
+
+}
---
+++ src/main/java/com/java4all/momo/responce/GlobalRollbackResponse.java
@@ -0,0 +1,9 @@
+package com.java4all.momo.responce;
+
+/**
+ * Global rollback response
+ * @author IT云清
+ */
+public class GlobalRollbackResponse {
+
+}
