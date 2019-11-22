bab770fdf023bc5a8d4be58ce6fcbb86862e8db4
ITyunqing <1186355422@qq.com>
2019-11-22 20:00:44 +0800

    mod interceptor

--- src/main/java/com/java4all/momo/core/GlobalTransactionalInterceptor.java
+++ src/main/java/com/java4all/momo/core/GlobalTransactionalInterceptor.java
@@ -1,5 +1,7 @@
 package com.java4all.momo.core;
 
+import com.java4all.momo.annotation.GlobalTransactional;
+import java.lang.reflect.Method;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 
@@ -13,7 +15,18 @@ public class GlobalTransactionalInterceptor implements MethodInterceptor{
     @Override
     public Object invoke(MethodInvocation methodInvocation) throws Throwable {
 
+        GlobalTransactional globalTransactional = this
+                .getGlobalTransactional(methodInvocation.getMethod());
+
 //TODO
         return null;
+    }
+    private GlobalTransactional getGlobalTransactional(Method method){
+        boolean globalTransactional = method.isAnnotationPresent(GlobalTransactional.class);
+        if(globalTransactional){
+            return method.getAnnotation(GlobalTransactional.class);
+        }else {
+            return null;
+        }
     }
 }
