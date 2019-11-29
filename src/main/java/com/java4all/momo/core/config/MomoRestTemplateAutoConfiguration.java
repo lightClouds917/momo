c772b4fe83361f19387383f344a03142034a7537
ITyunqing <1186355422@qq.com>
2019-11-29 22:19:01 +0800

    add MomoRestTemplateAutoConfiguration

--- src/main/java/com/java4all/momo/core/config/MomoRestTemplateAutoConfiguration.java
+++ src/main/java/com/java4all/momo/core/config/MomoRestTemplateAutoConfiguration.java
@@ -1,8 +1,15 @@
 package com.java4all.momo.core.config;
 
+import com.java4all.momo.core.interceptor.MomoRestTemplateInterceptor;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -16,9 +23,24 @@ public class MomoRestTemplateAutoConfiguration {
     private Collection<RestTemplate> restTemplates;
 
     @Autowired
-    private MomoProperties momoProperties;
+    private MomoRestTemplateInterceptor momoRestTemplateInterceptor;
 
-    public MomoRestTemplateAutoConfiguration() {
+    @Bean
+    public MomoRestTemplateInterceptor momoRestTemplateInterceptor(){
+        return new MomoRestTemplateInterceptor();
+    }
+
+    @PostConstruct
+    public void init(){
+        if(!CollectionUtils.isEmpty(restTemplates)){
+            Iterator<RestTemplate> iterator = restTemplates.iterator();
+            while (iterator.hasNext()){
+                RestTemplate restTemplate = iterator.next();
+                List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+                interceptors.add(this.momoRestTemplateInterceptor);
+                restTemplate.setInterceptors(interceptors);
+            }
+        }
 
     }
 }
