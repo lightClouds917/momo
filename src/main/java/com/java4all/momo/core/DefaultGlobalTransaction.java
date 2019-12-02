987718a08c38a5c17a4ec8beffe1ba2bca7f4621
ITyunqing <1186355422@qq.com>
2019-12-02 20:52:39 +0800

    mod DefaultGlobalTransaction

--- src/main/java/com/java4all/momo/core/DefaultGlobalTransaction.java
+++ src/main/java/com/java4all/momo/core/DefaultGlobalTransaction.java
@@ -8,8 +8,16 @@ import com.java4all.momo.constant.StoreMode;
  * @author IT云清
  * @date 2019年10月31日 21:26:25
  */
-public class DefaultGlobalTransaction extends GlobalTransaction{
+public class DefaultGlobalTransaction{
     private static final int DEFAULT_GLOBAL_TX_TIMEOUT = 60000;
+
+    private String xid;
+
+    private String txServiceGroup;
+
+    private String groupId;
+
+    private StoreMode storeMode = StoreMode.MEMORY;
 
     /**global transaction role*/
     private GlobalTransactionRole globalTransactionRole;
@@ -21,13 +29,14 @@ public class DefaultGlobalTransaction extends GlobalTransaction{
     public DefaultGlobalTransaction() {
     }
 
-    public DefaultGlobalTransaction(String xid,
-            GlobalTransactionRole globalTransactionRole,
-            GlobalStatus globalStatus,
-            StoreMode storeMode) {
-        this.setXid(xid);
+    public DefaultGlobalTransaction(String xid, String txServiceGroup, String groupId,
+            StoreMode storeMode, GlobalTransactionRole globalTransactionRole,
+            GlobalStatus globalStatus) {
+        this.xid = xid;
+        this.txServiceGroup = txServiceGroup;
+        this.groupId = groupId;
+        this.storeMode = storeMode;
         this.globalTransactionRole = globalTransactionRole;
         this.globalStatus = globalStatus;
-        this.setStoreMode(storeMode);
     }
 }
