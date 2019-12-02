5fa2d66fa60f9793d6b1202c45ce97322476e9e9
ITyunqing <1186355422@qq.com>
2019-12-02 20:26:55 +0800

    parse the GlobalTransactional

--- src/main/java/com/java4all/momo/core/interceptor/GlobalTransactionalInterceptor.java
+++ src/main/java/com/java4all/momo/core/interceptor/GlobalTransactionalInterceptor.java
@@ -1,9 +1,14 @@
 package com.java4all.momo.core.interceptor;
 
+import com.java4all.momo.annotation.GlobalTransactional;
+import com.java4all.momo.core.GlobalTransaction;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.core.BridgeMethodResolver;
+import org.springframework.util.ClassUtils;
 
 /**
  * Global Transactional Interceptor
@@ -14,9 +19,14 @@ public class GlobalTransactionalInterceptor implements MethodInterceptor{
 
     @Override
     public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+        //TODO 去了解
+        Class<?> targetClass = (methodInvocation.getThis()) == null ? null
+                : AopUtils.getTargetClass(methodInvocation.getThis());
+        Method mostSpecificMethod = ClassUtils
+                .getMostSpecificMethod(methodInvocation.getMethod(), targetClass);
+        final Method method = BridgeMethodResolver.findBridgedMethod(mostSpecificMethod);
+        final GlobalTransactional globalTransactional = this.getAnnotation(method, GlobalTransactional.class);
 
-
-//TODO
         return null;
     }
 
