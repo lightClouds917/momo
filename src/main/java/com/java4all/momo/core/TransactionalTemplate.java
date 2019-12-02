e52ee4767a2d0a80a80d540b85e375f6b8506a05
ITyunqing <1186355422@qq.com>
2019-12-02 20:52:39 +0800

    mod DefaultGlobalTransaction

--- src/main/java/com/java4all/momo/core/TransactionalTemplate.java
+++ src/main/java/com/java4all/momo/core/TransactionalTemplate.java
@@ -1,6 +1,8 @@
 package com.java4all.momo.core;
 
 import com.java4all.momo.context.GlobalTransactionContext;
+import com.java4all.momo.entity.TransactionInfo;
+import com.java4all.momo.exception.NeverHappenExcetion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,12 +13,29 @@ public class TransactionalTemplate {
     private static final Logger LOGGER = LoggerFactory.getLogger(TransactionalTemplate.class);
 
     public Object execute(TransactionalExecutor business)throws Throwable{
+        //1.get or create a transaction
         GlobalTransaction tx = GlobalTransactionContext.getCurrentOrNew();
+
+        //get transactionInfo
+        TransactionInfo transactionInfo = business.getTransactionInfo();
+
+        if(transactionInfo == null){
+            throw new NeverHappenExcetion("transaction does not exit");
+        }
+
+        //2.begin
+        this.beginTransaction(tx,transactionInfo);
+
+
 
 
 
         //TODO
         return null;
+    }
+
+    private void beginTransaction(GlobalTransaction tx, TransactionInfo transactionInfo) {
+
     }
 
 }
