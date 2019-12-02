d7280e4bc1c23a19eb23eabd67616e152db8a4c8
ITyunqing <1186355422@qq.com>
2019-12-02 20:48:00 +0800

    mod TransactionalExecutor

--- src/main/java/com/java4all/momo/core/TransactionalExecutor.java
+++ src/main/java/com/java4all/momo/core/TransactionalExecutor.java
@@ -1,9 +1,13 @@
 package com.java4all.momo.core;
 
+import com.java4all.momo.entity.TransactionInfo;
+
 /**
  * @author IT云清
  */
 public interface TransactionalExecutor {
 
     Object execute() throws Throwable;
+
+    TransactionInfo getTransactionInfo();
 }
