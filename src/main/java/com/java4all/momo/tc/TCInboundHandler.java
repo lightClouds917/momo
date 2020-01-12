74f91a3978032be5edb748abb35de365408844b7
ITyunqing <1186355422@qq.com>
2020-01-12 17:14:42 +0800

    update:add method

--- src/main/java/com/java4all/momo/tc/TCInboundHandler.java
+++ src/main/java/com/java4all/momo/tc/TCInboundHandler.java
@@ -1,13 +1,14 @@
 package com.java4all.momo.tc;
 
-import com.java4all.momo.request.branch.BranchRegistRequest;
 import com.java4all.momo.request.global.GlobalBeginRequest;
 import com.java4all.momo.request.global.GlobalCommitRequest;
 import com.java4all.momo.request.global.GlobalRegistRequest;
+import com.java4all.momo.request.global.GlobalReportRequest;
 import com.java4all.momo.request.global.GlobalRollbackRequest;
 import com.java4all.momo.responce.branch.BranchRegistResponse;
 import com.java4all.momo.responce.global.GlobalBeginResponse;
 import com.java4all.momo.responce.global.GlobalCommitResponse;
+import com.java4all.momo.responce.global.GlobalReportResponse;
 import com.java4all.momo.responce.global.GlobalRollbackResponse;
 import com.java4all.momo.session.BranchReportRequest;
 import com.java4all.momo.session.BranchReportResponse;
@@ -25,8 +26,12 @@ public interface TCInboundHandler {
 
     GlobalRollbackResponse handle(GlobalRollbackRequest  request);
 
+    GlobalReportResponse handle(GlobalReportRequest request);
+
     BranchRegistResponse handle(GlobalRegistRequest request);
 
     BranchReportResponse handle(BranchReportRequest request);
+
+    //todo globalstatus
 
 }
